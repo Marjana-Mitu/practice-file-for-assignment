d09db4079d3378d563a628288dd7b08f561f1f71
Marjana-Mitu <marjanamitu19@gmail.com>
2024-10-24 08:07:19 +0600

    first commit

---
+++ oct22.js
@@ -0,0 +1,14 @@
+// console.log ("welcome to JavaScript");
+// console.log ("welcome to practice101");
+// var name1 = "ABC Ahmed";
+// name1 = "PQR Khan";
+// console.log (name1)
+// name1 = "aiman"
+// console.log (name1)
+
+var studentName = prompt ("please enter your name here");
+var quiz1 =parseFloat(prompt ("Enter Quiz 1 Marks here"));
+var quiz2 = parseFloat(prompt ("Enter Quiz 2 Marks here"));
+var midterm = parseFloat(prompt("please endter yopur mid marks"));
+var totalMarks = quiz1 + quiz2 + midterm;
+console.log("Hello", studentName,"your total quiz marks is", totalMarks);
